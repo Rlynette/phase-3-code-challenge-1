9bf01eeb9971cdcaf13123534b662ef95fd8b3a6
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-10-31 14:00:34 +0300

    edit the console.rb file

--- tools/console.rb
+++ tools/console.rb
@@ -6,10 +6,10 @@ end
 
 
 ###  WRITE YOUR TEST CODE HERE ###
-
-
-
-
+at1=Author.new("Author1")
+mag1=Magazine.new("Mag1","Cat1")
+art1=Article.new(at1,mag1,"Title1")
+art2=Article.new(at1,mag1,"Title2")
 
 
 
