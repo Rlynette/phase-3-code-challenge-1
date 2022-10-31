ccd9e42cda609283ede8ead1097c6e7a1b1e5eb5
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-10-31 07:39:21 +0300

    complete Magazine deliverables

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,16 @@
 class Magazine
   attr_accessor :name, :category
 
-
+@@all = []
   def initialize(name, category)
     @name = name
     @category = category
+#add the initialised objects to a class variable called "all"
+ @@all << self
+end
 
-  end
-
+def self.all
+  @@all
+end
 
 end
