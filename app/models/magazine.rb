04d9c10597f1243feffe113b73bcd44cd150a0ab
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-10-31 15:17:52 +0300

    return array strings of titles of all articles in magazine

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -13,10 +13,22 @@ def self.all
   @@all
 end
 
+def magazine_articles
+  Article.all.filter{|article|article.magazine.name==@name}
+end
+
 def contributors
 #filter from articles the articles{|article|article.magazine.name==@name}
 #.map{|article|article.author.name}.uniq
-Article.all.filter{|article|article.magazine.name==@name}
+ magazine_articles
 .map{|article|article.author}.uniq
+end
+
+def self.find_by_name(name)
+  Magazine.all.find{|magazine|magazine.name==name}
+end
+
+def article_titles
+  magazine_articles.map{|article|article.title}
 end
 end
